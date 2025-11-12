db85ac098d41b475c4196bb4efdd71d6e73516bc
Diego Peña y Lillo <dpenaylilloluhrs@gmail.com>
2025-11-12 18:28:33 -0300

    Orden APIS

--- usuario-service/bin/main/com/clinica/api/usuario_service/dto/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.clinica.api.usuario_service.dto
-
-data class LoginRequest(
-    val correo: String,
-    val contrasena: String
-)
--- usuario-service/bin/main/com/clinica/api/usuario_service/dto/LoginResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.clinica.api.usuario_service.dto
-
-data class LoginResponse(
-    val authenticated: Boolean,
-    val message: String,
-    val usuario: UsuarioResponse? = null
-)
